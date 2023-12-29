75fe12c7b64763506e38e9ebe08cd19540503d3d
Akshat Singh <HackerDaGreat57@gmail.com>
2023-12-29 14:02:33 -0800

    optimizeatioun

--- src/main.c
+++ src/main.c
@@ -21,27 +21,30 @@
 const uint LED_CPU0 = 9;
 const uint LED_CPU1 = 5;
 
+//char printmsg[];
+
 void core1() {
-  multicore_fifo_push_blocking(FLAG_VALUE);
-  uint32_t g = multicore_fifo_pop_blocking();
-  if (g != FLAG_VALUE) {
-    printf("\033[1;101mERR:\033[0m Something's wrong on core 1??\n");
-  } else {
-    printf("\033[1;105mSETUP:\033[0m Core 1 initialized.\n");
-  }
+  //printf(printmsg);
+  // multicore_fifo_push_blocking(FLAG_VALUE);
+  // uint32_t g = multicore_fifo_pop_blocking();
+  // if (g != FLAG_VALUE) {
+  //   printf("\033[1;101mERR:\033[0m Something's wrong on core 1??\n");
+  // } else {
+  //   printf("\033[1;105mSETUP:\033[0m Core 1 initialized.\n");
+  // }
 
-  printf("\033[1;92mCPU1:\033[0m Hey there!\n");
+  // printf("\033[1;92mCPU1:\033[0m Hey there!\n");
 
-  uint64_t counter1 = 0;
-  while(true) {
-    gpio_put(LED_CPU1, 1);
+  // uint64_t counter1 = 0;
+  // while(true) {
+  //   gpio_put(LED_CPU1, 1);
 
-    //printf("\033[1;92mCPU1:\033[0m Hello world! %lld\n", counter1);
+  //   //printf("\033[1;92mCPU1:\033[0m Hello world! %lld\n", counter1);
 
-    counter1++;
-    gpio_put(LED_CPU1, 0);
-    //sleep_us(1);
-  }
+  //   counter1++;
+  //   gpio_put(LED_CPU1, 0);
+  //   //sleep_us(1);
+  // }
 }
 
 int main() {
@@ -70,24 +73,25 @@ int main() {
   gpio_set_dir(LED_CPU0, GPIO_OUT);
   gpio_set_dir(LED_CPU1, GPIO_OUT);
 
-  printf("\033[1;105mSETUP:\033[0m Initializing core 1...\n");
-  multicore_launch_core1(core1);
-  uint32_t g = multicore_fifo_pop_blocking();
-  printf("\033[1;105mSETUP:\033[0m Initializing core 0...\n");
-  if (g != FLAG_VALUE) {
-    printf("\033[1;101mERR:\033[0m Something's wrong on core 0??\n");
-  } else {
-    multicore_fifo_push_blocking(FLAG_VALUE);
-    printf("\033[1;105mSETUP:\033[0m Core 0 initialized.\n");
-  }
+  // printf("\033[1;105mSETUP:\033[0m Initializing core 1...\n");
+  // multicore_launch_core1(core1);
+  // uint32_t g = multicore_fifo_pop_blocking();
+  // printf("\033[1;105mSETUP:\033[0m Initializing core 0...\n");
+  // if (g != FLAG_VALUE) {
+  //   printf("\033[1;101mERR:\033[0m Something's wrong on core 0??\n");
+  // } else {
+  //   multicore_fifo_push_blocking(FLAG_VALUE);
+  //   printf("\033[1;105mSETUP:\033[0m Core 0 initialized.\n");
+  // }
 
   printf("\033[1;91mCPU0:\033[0m Hiya!\n");
 
-  uint64_t counter0 = 0;
+  uint_fast64_t counter0 = 0;
   while(true) {
     gpio_put(LED_CPU0, 1);
 
-    printf("\033[1;91mCPU0:\033[0m Hello world! %lld\n", counter0);
+    //printf("\033[1;91mCPU0:\033[0m Hello world! %lld\n", counter0);
+    printf("%lld\n", counter0);
 
     counter0++;
     gpio_put(LED_CPU0, 0);
